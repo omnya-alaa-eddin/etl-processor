8404f72e0cd90723d4024ece6e43e346c1135bb0
oali5 <oali5@dxc.com>
2022-01-18 22:15:33 +0200

    Add used liquidity script

---
+++ etl-processor/src/main/resources/product-liquidity.sql
@@ -0,0 +1,62 @@
+
+CREATE TABLE PRODUCT_LIQUIDITY (
+p_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+p_order INT(10) NOT NULL,
+tenant_id INT(10) NOT NULL,
+p_name varchar(10)
+) ;
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(1,1,'c1','Retail Savings');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(2,7,'c1','Registered 4+ Year');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(3,5,'c1','TFSA HISA');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(4,2,'c1','Retail Chequing');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(5,11,'c1','Retail Chequing Other');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(6,22,'c1','Registered 20 month');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(7,9,'c1','StepUp 6Plus6');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(8,4,'c1','Registered Savings');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(9,10,'c1','Retail 1 Year');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(10,21,'c1','StepUp 9Plus9');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(11,3,'c1','PRIMECONNECT');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(12,6,'c1','Registered 1 Year');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(13,15,'c1','Registered StepUp 9Plus9');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(14,12,'c1','Registered Short Term Non-Redeemable');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(15,23,'c1','Registered 12 Month Cashable');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(16,8,'c1','Retail HISA');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(17,14,'c1','StepUp 18Plus18');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(18,27,'c1','Registered HISA');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(19,25,'c1','TFSA StepUp 9Plus9');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(20,16,'c1','Retail 2 Year');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(21,17,'c1','Registered StepUp 18Plus18');
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(22,19,'c1','12 Month Cashable');
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(23,20,'c1','Registered StepUp 6Plus6');
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(24,26,'c1','Retail 3 Year');
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(25,13,'c1','USD Retail Chequing');
+
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values (26,24,'c1','Registered PRIMECONNECT');
+
+
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(27,18,'c1','TFSA StepUp 18Plus18');
+
+
+
+INSERT INTO `products-schema`.`PRODUCT_LIQUIDITY`(`p_id`,`p_order`,`tenant_id`,`p_name`) values(28,28,'c1','NA');
